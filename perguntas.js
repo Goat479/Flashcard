58b5f7b329119410c0b76cd06c969550f2350c0b
Goat479 <bruno.henrique.verissimo@escola.pr.gov.br>
2025-08-14 14:42:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'História'
+    'Com quantos anos o imperador D. Pedro II assumiu o reinado?'
+    '14 anos'
+)
+criaCartao(
+    'Geografia'
+    'Quantos estados tem o nordeste?'
+    '9 estados'
+)
+criaCartao(
+    'Filosofia'
+    'Quem disse "o homem é um animal político"?'
+    'Aristóteles'
+)
+criaCartao(
+    'Sociologia'
+    'Ideologia desenvolvida por Karl Marx que marcou o século XX'
+    'Marxismo/Comunismo'
+)
+criaCartao(
+    'Biologia'
+    'Qual o livro de Charles Darwin que revolucionou a biologia?'
+    'A Origem das Espécies'
+)
+criaCartao(
+    'Química'
+    'Qual a fórmula do carboidrato?'
+    'CH²O'
+)
+criaCartao(
+    'Matemática'
+    'Na equação "23+25.2-5 = X" qual o valor de X?'
+    '68'
+)
+criaCartao(
+    'Português'
+    'Belo e bonito são sinônimos ou antônimos?'
+    'Sinônimos'
+)
+criaCartao(
+    'Esportes'
+    'Qual o clube brasileiro com 3 mundiais'
+    'O palmeiras é que não é'
+)
